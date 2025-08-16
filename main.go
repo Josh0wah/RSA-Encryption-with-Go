d02646462139ee991785d0b150dad4e3682c0151
Joshua Singleton <134548428+Josh0wah@users.noreply.github.com>
2025-08-16 02:14:07 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -4,10 +4,22 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 	"os"
 	"strconv"
 )
+
+func main() {
+	switch os.Args[1] {
+	case "-e":
+		encrypt(string(os.Args[2]))
+	case "-d":
+		decrypt(string(os.Args[2]), os.Args[3], os.Args[4])
+	default:
+		panic("Invalid command")
+	}
+}
 
 func findPrime() uint64 {
 	testNum := rand.Uint64()%100 + 10
@@ -26,15 +38,7 @@ func gcd(a, b uint64) uint64 {
 	return gcd(b, a%b)
 }
 
-func encrypt(fileName string, p uint64, q uint64, n uint64, phi uint64) {
-
-}
-
-func decrypt(fileName string) {
-
-}
-
-func main() {
+func encrypt(fileName string) {
 	p := findPrime()
 	q := findPrime()
 	for q == p {
@@ -55,7 +59,7 @@ func main() {
 		}
 	}
 
-	fKeys, err := os.Create("test/keys.txt")
+	fKeys, err := os.Create("keys.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -67,4 +71,46 @@ func main() {
 		panic(err)
 	}
 
+	message, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	toInt, _ := strconv.Atoi(string(message))
+	encrypted := uint64(math.Pow(float64(toInt), float64(e))) % n
+
+	fEncrypted, err := os.Create("encrypted.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fEncrypted.Close()
+
+	_, err = fEncrypted.WriteString(strconv.FormatUint(encrypted, 10) + "\n")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func decrypt(fileName string, d string, n string) {
+	dInt, _ := strconv.Atoi(d)
+	nInt, _ := strconv.Atoi(n)
+
+	encrypted, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	toInt, _ := strconv.Atoi(string(encrypted))
+	decrypted := uint64(math.Pow(float64(toInt), float64(dInt))) % uint64(nInt)
+
+	fDecrypted, err := os.Create("decrypted.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fDecrypted.Close()
+
+	_, err = fDecrypted.WriteString(strconv.FormatUint(decrypted, 10) + "\n")
+	if err != nil {
+		panic(err)
+	}
 }
